b6435dd01570fb969cc023f15be8651ff5f75a8d
KarolisTru <83576019+KarolisTru@users.noreply.github.com>
2021-05-31 14:56:47 +0300

    latest version of tests

--- src/app/core/product-form.service.spec.ts
+++ src/app/core/product-form.service.spec.ts
@@ -3,7 +3,6 @@ import { TestBed } from '@angular/core/testing';
 import { ProductFormService } from './product-form.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
-
 describe('ProductFormService', () => {
   let productFormService: ProductFormService;
   let productFormServiceSpy: jasmine.SpyObj<FormBuilder>;
@@ -15,43 +14,49 @@ describe('ProductFormService', () => {
       providers: [ProductFormService, { provide: FormBuilder, useValue: spy }],
     });
     productFormService = TestBed.inject(ProductFormService);
-    productFormServiceSpy = TestBed.inject(FormBuilder) as jasmine.SpyObj<FormBuilder>;
+    productFormServiceSpy = TestBed.inject(
+      FormBuilder
+    ) as jasmine.SpyObj<FormBuilder>;
   });
 
   it('generateFormValidation() should return a stubbed value when the argument object is provided (relevant for editItemForm)', () => {
-    const fb = new FormBuilder;
+    const fb = new FormBuilder();
     const argumentObject = {
       name: 'Name Example',
       photoUrl: 'Photo URL Example',
       price: 25.99,
       productDescription: 'Lorem Ipsum',
       flagged: true,
-      id: 20
-    }
+      id: 20,
+    };
     const stubValue = fb.group(argumentObject);
     productFormServiceSpy.group.and.returnValue(stubValue);
 
-    expect(productFormService.generateFormValidation(argumentObject)).toBe(stubValue);
+    expect(productFormService.generateFormValidation(argumentObject)).toBe(
+      stubValue
+    );
     expect(productFormServiceSpy.group.calls.count()).toBe(1);
-    expect(productFormServiceSpy.group.calls.mostRecent().returnValue)
-    .toBe(stubValue);
-  })
-  
+    expect(productFormServiceSpy.group.calls.mostRecent().returnValue).toBe(
+      stubValue
+    );
+  });
+
   it('generateFormValidation() should return a defaultValue when no argument is provided (relevant for addItemForm)', () => {
-    const fb = new FormBuilder;
+    const fb = new FormBuilder();
     const defaultValue = {
       name: '',
       photoUrl: '',
       price: 0,
       productDescription: '',
       flagged: false,
-    }
+    };
     const stubValue = fb.group(defaultValue);
     productFormServiceSpy.group.and.returnValue(stubValue);
 
     expect(productFormService.generateFormValidation()).toBe(stubValue);
     expect(productFormServiceSpy.group.calls.count()).toBe(1);
-    expect(productFormServiceSpy.group.calls.mostRecent().returnValue)
-    .toBe(stubValue);
-  })
+    expect(productFormServiceSpy.group.calls.mostRecent().returnValue).toBe(
+      stubValue
+    );
+  });
 });
--- src/app/shared/components/carousel/carousel.component.spec.ts
+++ src/app/shared/components/carousel/carousel.component.spec.ts
@@ -1,25 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Component } from '@angular/core';
 import { CarouselComponent } from './carousel.component';
+import { CarouselItem } from '../../../carousel-item.interface';
 
-describe('CarouselComponent', () => {
-  let component: CarouselComponent;
-  let fixture: ComponentFixture<CarouselComponent>;
+@Component({
+  template: ` <carousel [items]="items"></carousel>`,
+})
+class TestHostComponent {
+  items: CarouselItem = 
+    {
+      id: 'carousel-item-1',
+      photoUrl: 'https://placeimg.com/640/480/tech',
+      link: '#',
+      text: 'Priority 1',
+      priority: 1,
+    }  
+}
+
+describe('CarouselComponent when tested inside host', () => {
+  let testHost: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
+  let carouselEl: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CarouselComponent ]
-    })
-    .compileComponents();
+      declarations: [CarouselComponent, TestHostComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CarouselComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(TestHostComponent);
+    testHost = fixture.componentInstance;
+    carouselEl = fixture.nativeElement.querySelector('.carousel')
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  //not working yet
+  it('should display the correct text', () => {
+    fixture.detectChanges();
+    const expectedText = testHost.items.text;
+    expect(carouselEl.textContent).toContain(expectedText);
+  })
 });
